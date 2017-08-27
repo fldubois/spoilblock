ecf28e6b8f9b8ba3a0bf599e70c66f524f289233
Florent Dubois <florent.dubois@devaddict.io>
2017-08-27 23:24:02 +0200

    Add selection box

--- extension/js/selector.js
+++ extension/js/selector.js
@@ -1,11 +1,33 @@
 'use strict';
 
 {
-  let layer = document.createElement('div');
+  const select = document.createElement('div');
+  const layer  = document.createElement('div');
+
+  select.classList.add('spoilblock-select-box');
 
   layer.classList.add('spoilblock-select-layer');
+  layer.appendChild(select);
 
   document.body.appendChild(layer);
+
+  document.body.addEventListener('mouseover', function (event) {
+    let target  = event.target;
+    let display = (target.currentStyle ? target.currentStyle : getComputedStyle(target, null)).display;
+
+    while (display !== 'block') {
+      target  = target.parent;
+      display = (target.currentStyle ? target.currentStyle : getComputedStyle(target, null)).display;
+    }
+
+    const rect = target.getBoundingClientRect();
+
+    select.style.top  = (window.scrollY + rect.top)  + 'px';
+    select.style.left = (window.scrollX + rect.left) + 'px';
+
+    select.style.width  = rect.width  + 'px';
+    select.style.height = rect.height + 'px';
+  });
 
   browser.runtime.onMessage.addListener((message) => {
     if (typeof message === 'object' && message.action === 'disable') {
