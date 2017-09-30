c454302c31ca92f3a66881f6e2577d865acabf78
Florent Dubois <florent.dubois@devaddict.io>
2017-09-30 23:50:45 +0200

    Create logs directory if not exists

--- api/lib/common/logger.js
+++ api/lib/common/logger.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const fs   = require('fs');
 const path = require('path');
 
 const bunyan = require('bunyan');
@@ -8,6 +9,14 @@ const config = require('config');
 const pkg = require('../../package.json');
 
 const level = process.env.LOG_LEVEL || (config.has('logger.level') ? config.get('logger.level') : 'info');
+
+try {
+  fs.access(config.get('logger.path'), fs.constants.R_OK | fs.constants.W_OK);
+} catch (error) {
+  if (error.code === 'ENOENT') {
+    fs.mkdirSync(config.get('logger.path'));
+  }
+}
 
 module.exports = bunyan.createLogger({
   name:  pkg.name,
